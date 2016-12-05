0e02cdda148c9a93a5207f6dc43a7dfe353b4478
Alexis Zorba <zorba@nat-oitwireless-inside-vapornet100-a-16885.Princeton.EDU>
2016-12-05 12:59:26 -0500

    improved load_data.py

--- load_data.py
+++ load_data.py
@@ -55,8 +55,50 @@ def filter_data(data):
 all_trials  = {}
 for r in rows:
 	rs.append(r)
-	data=json.loads(r['datastring'])
-	workerID = data["workerId"]
-	trials_data = get_trials_data(data)
-	all_trials[workerID] = filter_data(trials_data)
+	if r["datastring"]:
+		data=json.loads(r['datastring'])
+		workerID = data["workerId"]
+		trials_data = get_trials_data(data)
+		all_trials[workerID] = filter_data(trials_data)
+
+good_workers = [5]
+
+X=[]
+T_c=[]
+T_p = []
+NT_c = []
+NT_p = []
+D=[]
+C=[]
+for wid in good_workers:
+	x=[]
+	t_c=[]
+	t_p=[]
+	nt_c=[]
+	nt_p=[]	
+	d=[]
+	c=[]
+	for trial in all_trials[wid]:
+		load = len(trial)-7
+		x.append(trial["report_color"])
+		t_c.append(trials["T_color"])
+		t_p.append(trials["T_pos"])
+		d.append(trials["delay"])
+		c.append(trials["show"])
+		for nt in range(load):
+			nt_c.append(trials["NT_color"+str(nt)])
+			nt_pos.append(trials["NT_pos"+str(nt)])
+	X.append(x)
+	T_c.append(t_c)
+	T_pos.append(t_p)
+	NT_c.append(nt_c)
+	NT_pos.append(nt_p)
+	D.append(d)
+	C.append(c)
+
+
+
+
+
+
 
