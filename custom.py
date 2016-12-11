70a859fa934801d31861bfc2231fc5fcf30cbebd
Alexis Zorba <zorba@Alexiss-MBP-2.westell.com>
2016-12-11 14:14:53 -0500

    rwd

--- custom.py
+++ custom.py
@@ -161,7 +161,7 @@ def compute_bonus():
         for report in data: # for line in data file
             trial = report['trialdata']
             if trial["phase"]==TASK:
-                bonus=round(trial["total_reward"],2)
+                bonus+=trial["trial_rwd"]
 
         print "bonus: ",bonus
         user.bonus = bonus
